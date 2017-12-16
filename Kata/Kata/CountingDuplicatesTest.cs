0e99e5068d9aed3dbc84f418252e446e1e84914a
Cloud <thirmoon@gmail.com>
2017-12-16 16:17:51 +0800

    added test case

---
+++ Kata/Kata/CountingDuplicatesTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataProd;
+
+namespace Kata
+{
+    [TestClass]
+    public class CountingDuplicatesTest
+    {
+        [TestMethod]
+        public void CountingDuplicate_abcde()
+        {
+            var Counter = new CountingDuplicates();
+            string input = "abcde";
+            int expect = 0;
+            Assert.AreEqual(expect, Counter.Calculator(input));
+        }
+    }
+}
---
+++ Kata/KataProd/CountingDuplicates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataProd
+{
+    public class CountingDuplicates
+    {
+        public int Calculator(string input)
+        {
+            return 0;
+        }
+    }
+}
